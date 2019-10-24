ffa3ef7c2898e95095ecd2a4a86f65afa7f85142
Martin <martin.goransson94@gmail.com>
2019-10-24 09:21:42 +0200

    Initial commit

---
+++ Bakery/Cake.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Bakery
+{
+    public class Cake
+    {
+        string orderNo;
+
+    }
+
+    public interface IPaymentHandler {
+        void AcceptPayment();
+        void PayBakers();
+    }
+
+    public abstract class Human
+    {
+        string firstName;
+        string lastName;
+
+        public abstract void ProcessOrder(Order order);
+    }
+
+    public class Baker : Human
+    {
+        char quality;
+
+        public override void ProcessOrder(Order Order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Customer : Human
+    {
+
+        public override void ProcessOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class Order
+    {
+
+    }
+
+    public class Ingredient
+    {
+
+    }
+
+    public class Payments : IPaymentHandler
+    {
+        public void AcceptPayment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PayBakers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimulationNation/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleSimulationEngine2000;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimulationNation
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new TextInput();
+            var gui = new ConsoleGUI() { Input = input };
+            var sim = new MySimulation(gui, input);
+            await gui.Start(sim);
+        }
+    }
+
+    public class MySimulation : Simulation
+    {
+        private ConsoleGUI gui;
+        private TextInput input;
+
+        public MySimulation(ConsoleGUI gui, TextInput input)
+        {
+            this.gui = gui;
+            this.input = input;
+        }
+
+        public override List<BaseDisplay> Displays => throw new NotImplementedException();
+
+        public override void PassTime(int deltaTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
