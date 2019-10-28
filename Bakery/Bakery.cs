e12139d62df81a3ef5e0d17873418d0ab59fdd0c
Martin <martin.goransson94@gmail.com>
2019-10-28 10:00:04 +0100

    Some Updates

---
+++ Bakery/Bakery.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Bakery
+{
+    public class Bakery
+    {
+
+        List<Baker> listOfBakers = new List<Baker>();
+
+        List<Customer> listofCustomers = new List<Customer>();
+
+    }
+}
--- Bakery/Order.cs
+++ Bakery/Order.cs
@@ -4,8 +4,28 @@ namespace Bakery
 {
     public class Order
     {
-        public int orderNo;
         public List<Cake> cakeOrder = new List<Cake>();
+
+        public int CalcTotalCost(List<Cake> cakeList) {
+            int returnValue=0;
+            foreach (Cake c in cakeList) {
+                returnValue = returnValue + c.Cost;
+            }
+
+            return returnValue;
+        }
+
+        public int CalcTotalBakeTime(List<Cake> cakeList)
+        {
+            int returnValue = 0;
+            foreach (Cake c in cakeList)
+            {
+                returnValue = returnValue + c.BakeTime;
+            }
+
+            return returnValue;
+        }
+
 
     }
 }
